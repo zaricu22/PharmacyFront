95e64d987c6860fad584b9b4b6062ab6729e11a4
zaricu22 <48631871+zaricu22@users.noreply.github.com>
2024-02-12 12:51:09 +0100

    docs only

--- src/app/interfaces/iproduct.ts
+++
@@ -1,9 +0,0 @@
-import {IManufacturer} from "./imanufacturer";
-
-export interface IProduct {
-  id: string;
-  name: string;
-  manufacturer: IManufacturer;
-  price: number;
-  expiryDate: Date;
-}
